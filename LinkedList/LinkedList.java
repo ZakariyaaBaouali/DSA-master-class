dfe47e61b50bda90e87595564d683ed7c7e22901
zakaria <zakariabaouali@outlook.com>
2024-11-11 09:14:41 +0100

    impl addLast and toString methods for LinkedList

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class LinkedList<T> {
+
+    private class Node {
+        Object value;
+        Node next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    public void addLast(T item) {
+        var node = new Node(item);
+
+        if (this.isEmpty())
+            this.first = this.last = node;
+        else {
+            this.last.next = node;
+            this.last = node;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<Object> items = new ArrayList<>();
+        var cuurent = this.first;
+        while (cuurent != null) {
+            items.add(cuurent.value);
+            cuurent = cuurent.next;
+        }
+        return items.toString();
+    }
+
+    private boolean isEmpty() {
+        return this.first == null;
+    }
+}
---
+++ LinkedList/Main.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi LinkedList 🚀🚀🚀");
+
+        LinkedList<Integer> numbers = new LinkedList<Integer>();
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+
+        System.out.println(numbers);
+    }
+
+}
