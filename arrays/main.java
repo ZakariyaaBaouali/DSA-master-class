3939b4c04891bf840286d40ed5b7cbf13203eb40
zakaria <zakariabaouali@outlook.com>
2024-11-11 08:42:53 +0100

    implement insert and toString methods with array

---
+++ arrays/main.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hi Arrays 🚀🚀🚀");
+
+        Array<String> names = new Array<String>(2);
+        names.insert("zakaria");
+        names.insert("tesla");
+        names.insert("meta");
+
+        System.out.println(names);
+    }
+}
