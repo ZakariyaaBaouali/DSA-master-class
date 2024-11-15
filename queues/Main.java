316453b29fe7715168b0d5dcf741515c9f5bc75f
zakaria <zakariabaouali@outlook.com>
2024-11-15 06:27:22 +0100

    java impl for queues

--- queues/Main.java
+++ queues/Main.java
@@ -1,5 +1,9 @@
 package queues;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to queues ,  FIFO 🚀🚀🚀");
@@ -21,5 +25,28 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        // JAVA IMPLEMENTATION FOR QUEUE
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        reverseQueue(queue);
+    }
+
+    public static void reverseQueue(Queue<Integer> q) {
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        while (!q.isEmpty()) {
+            stack.push(q.remove());
+        }
+
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+
+        System.out.println("reversed Queue : " + queue);
     }
 }
