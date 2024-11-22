ea22c3c498934458c96e95e93e54985a6b4344d5
zakaria <zakariabaouali@outlook.com>
2024-11-22 07:51:55 +0100

    refactor previous code

--- AVLTrees/AVLTree.java
+++ AVLTrees/AVLTree.java
@@ -36,17 +36,12 @@ public class AVLTree {
         else
             root.leftChild = this.insert(root.leftChild , value);
 
+        root.height = Math.max(this.height(root.leftChild) , this.height(root.rightChild)) + 1;
 
-        var leftHeight = this.height(root.leftChild);
-        var rightHeight = this.height(root.rightChild);
-
-        root.height = Math.max(leftHeight , rightHeight) + 1;
-
-        var balanceFactor = leftHeight - rightHeight;
-        if(balanceFactor > 1)
-            System.out.println(root.value + " is left heavy");
-        else if(balanceFactor < -1)
+        if(this.isRightHeavy(root))
             System.out.println(root.value + " is right heavy");
+        else if(this.isLeftHeavy(root))
+            System.out.println(root.value + " is left heavy");
 
         return root;
     }
@@ -59,4 +54,15 @@ public class AVLTree {
         return node == null ? -1 : node.height;
     }
 
+    private int balanceFactor(AVLNode node){
+        return this.height(node.leftChild) - this.height(node.rightChild);
+    }
+
+    private boolean isLeftHeavy(AVLNode node){
+        return this.balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node){
+        return this.balanceFactor(node) < -1;
+    }
 }
